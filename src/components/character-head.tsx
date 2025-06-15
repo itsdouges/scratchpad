a3d37f3b90509248abb6f45d66823ed68507c113
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-06-15 19:33:31 +1000

    chore: dot product the directions

--- src/components/character-head.tsx
+++ src/components/character-head.tsx
@@ -3,6 +3,9 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { Group, Vector3 } from "three";
 
+const v1 = new Vector3();
+const v2 = new Vector3();
+
 export function CharacterHead() {
   const camera = useThree((state) => state.camera);
   const surfaces = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -14,8 +17,16 @@ export function CharacterHead() {
       return;
     }
 
-    const direction = ref.current.getWorldDirection(new Vector3()).normalize();
-    const angle = Math.atan2(direction.x, direction.z) + Math.PI / 8; // Offset to match the surface numbering
+    const cameraDirection = camera.getWorldDirection(v1).normalize().negate();
+    const characterDirection = ref.current.getWorldDirection(v2).normalize();
+
+    const angle = Math.atan2(
+      cameraDirection.x * characterDirection.z -
+        cameraDirection.z * characterDirection.x,
+      cameraDirection.x * characterDirection.x +
+        cameraDirection.z * characterDirection.z
+    );
+
     const index = Math.floor((angle / (Math.PI * 2)) * surfaces.length);
     const nextSurface = surfaces.at(index);
 
