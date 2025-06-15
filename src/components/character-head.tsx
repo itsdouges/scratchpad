d0099db1e9a3e2eea65e7a033a5f4fc8ddace613
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-06-16 09:58:29 +1000

    feat: get head working

--- src/components/character-head.tsx
+++ src/components/character-head.tsx
@@ -1,48 +1,109 @@
-import { Billboard, Text } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useRef, useState } from "react";
-import { Group, Vector3 } from "three";
+import { useTexture, Billboard } from "@react-three/drei";
+import { useReducer, useRef, useState } from "react";
+import { Group, Vector3, MathUtils, SRGBColorSpace } from "three";
 
 const v1 = new Vector3();
 const v2 = new Vector3();
 
+interface FaceRenderable {
+  index: number;
+  reverse: boolean;
+}
+
+function faceReducer(
+  prevState: FaceRenderable | undefined,
+  nextState: FaceRenderable | undefined
+): FaceRenderable | undefined {
+  if (nextState === undefined || prevState === undefined) {
+    return nextState;
+  }
+
+  if (
+    prevState.index === nextState.index &&
+    prevState.reverse === nextState.reverse
+  ) {
+    return prevState;
+  }
+
+  return nextState;
+}
+
 export function CharacterHead() {
   const camera = useThree((state) => state.camera);
-  const surfaces = [1, 2, 3, 4, 5, 6, 7, 8];
-  const [surface, setSurface] = useState<number | undefined>(surfaces[0]);
   const ref = useRef<Group>(null);
+  const [face, setFace] = useReducer(faceReducer, { index: 0, reverse: false });
+  const textures = useTexture(
+    [
+      "/assets/faces/messy/face_1.png",
+      "/assets/faces/messy/face_2.png",
+      "/assets/faces/messy/face_3.png",
+      "/assets/faces/messy/face_4.png",
+    ],
+    (textures) =>
+      textures.forEach((texture) => {
+        texture.colorSpace = SRGBColorSpace;
+      })
+  );
 
   useFrame(() => {
     if (!ref.current) {
       return;
     }
 
-    const cameraDirection = camera.getWorldDirection(v1).normalize().negate();
-    const characterDirection = ref.current.getWorldDirection(v2).normalize();
+    const cameraDirection = camera
+      .getWorldDirection(v1)
+      .setY(0)
+      .normalize()
+      .negate();
+    const characterDirection = ref.current
+      .getWorldDirection(v2)
+      .setY(0)
+      .normalize();
 
-    const angle = Math.atan2(
-      cameraDirection.x * characterDirection.z -
-        cameraDirection.z * characterDirection.x,
-      cameraDirection.x * characterDirection.x +
-        cameraDirection.z * characterDirection.z
+    const angle = MathUtils.radToDeg(
+      Math.atan2(
+        cameraDirection.x * characterDirection.z -
+          cameraDirection.z * characterDirection.x,
+        cameraDirection.x * characterDirection.x +
+          cameraDirection.z * characterDirection.z
+      )
     );
+    const absoluteAngle = Math.abs(angle);
 
-    const index = Math.floor((angle / (Math.PI * 2)) * surfaces.length);
-    const nextSurface = surfaces.at(index);
+    let nextIndex: number | undefined;
 
-    setSurface(nextSurface);
+    if (absoluteAngle < 20 && absoluteAngle >= 0) {
+      nextIndex = 0;
+    } else if (absoluteAngle < 80 && absoluteAngle >= 20) {
+      nextIndex = 1;
+    } else if (absoluteAngle < 120 && absoluteAngle >= 80) {
+      nextIndex = 2;
+    } else if (absoluteAngle < 160 && absoluteAngle >= 120) {
+      nextIndex = 3;
+    }
+
+    if (typeof nextIndex === "number") {
+      setFace({ index: nextIndex, reverse: angle < 0 });
+    } else {
+      setFace(undefined);
+    }
   });
 
   return (
     <>
       <group ref={ref} />
       <Billboard>
-        <mesh position={[0, 0.88, 0]}>
-          <planeGeometry />
-          <Text color="black" position={[0, 0, 0.02]}>
-            {surface}
-          </Text>
-        </mesh>
+        <group position={[0, 0.88, 0]}>
+          <mesh scale={[face?.reverse ? -1 : 1, 1, 1]} visible={!!face}>
+            <planeGeometry />
+            <meshBasicMaterial
+              depthTest={false}
+              map={face ? textures[face.index] : undefined}
+              transparent
+            />
+          </mesh>
+        </group>
       </Billboard>
     </>
   );
--- src/components/character.tsx
+++ src/components/character.tsx
@@ -8,6 +8,10 @@ export function Character() {
         <boxGeometry args={[0.3, 0.8, 0.3]} />
         <meshStandardMaterial color="red" />
       </mesh>
+      <mesh castShadow position={[0, 0.4, 0]} receiveShadow>
+        <boxGeometry args={[1, 0.3, 0.3]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
     </group>
   );
 }
